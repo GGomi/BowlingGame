33bc93f788bc026193e5f0a0c779861412de0836
ggomi <dhkdn4042@naver.com>
2019-01-03 14:48:27 +0900

    Modified getScore & roll
    - 각 프레임의 스코어를 배열로 받고, score에 합해주는 방식으로 변경.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,29 @@
 public class Game {
 
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
 
     public void roll(int pins) {
-        this.score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public Integer getScore() {
-        return this.score;
+        int score = 0;
+
+        for(int frame = 0; frame < 10; frame++) {
+            int firstRollInFrame = frame* 2;
+            if(isSpare(firstRollInFrame)) {
+                score += 10 + rolls[firstRollInFrame + 2];
+            } else {
+                score += rolls[frame * 2] + rolls[frame * 2 + 1];
+            }
+        }
+
+        return score;
+    }
+
+    private boolean isSpare(int firstRollInFrame) {
+        return rolls[firstRollInFrame] + rolls[firstRollInFrame + 1] == 10;
     }
 }
